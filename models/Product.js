8a5565edc0af8ac7711f6edf97a6557b3990a345
eramos3 <cisco@Eders-MacBook-Pro.local>
2021-01-21 11:44:30 -0800

    updated readme

--- models/Product.js
+++ models/Product.js
@@ -30,7 +30,7 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      values: "10",
+      defaultValue: 10,
       validate: {
         isNumeric: true
       }
